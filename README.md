88d789245326df732b97ae887eb8e7777cf61e1f
Alpha2-5232 <79903261+Alpha2-5232@users.noreply.github.com>
2021-07-14 20:49:48 +0100

    added essential files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nodeCellMachine
 Some tools in node.js for Mystic's Mod
+
+## Stuff that should be in the .env file
+ - port: the port to listen on
---
+++ index.js
@@ -0,0 +1,12 @@
+const dotenv  = require('dotenv')
+dotenv.config()
+var http = require('http')
+var server = http.createServer(function (req, res) {   // 2 - creating server
+
+    //handle incomming requests here..
+
+});
+
+server.listen(process.env.PORT); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port ${process.env.PORT} is running..')
