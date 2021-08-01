afb96c15a87a898390af7f0543c2061ef957e5b6
Alpha2-5232 <79903261+Alpha2-5232@users.noreply.github.com>
2021-08-01 18:41:55 +0100

    asdf

--- primes.js
+++ primes.js
@@ -8,7 +8,8 @@ while(primesFound < totalPrimes) {
 function isPrime(n) {
 	let i = 0
 	while((allPrimes[i] < Math.ceil(sqrt(n)) )&& (i < allPrimes.length)) {
-
+		if(n % allPrimes[i]) return false;
+		i++
 	}
-	return;
+	return false;
 }
